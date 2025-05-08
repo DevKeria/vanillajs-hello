7659ada0a643da1ae911cdcc6982cbe5adf93ef3
DevKeria <keriawiley@yahoo.com>
2025-05-08 23:19:59 +0000

    changes to domain name

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateDomainNames() {
+  let pronouns = ['the', 'our'];
+  let adjectives = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+
+  let domainNames = [];
+
+  pronouns.forEach(pronoun => {
+    adjectives.forEach(adj => {
+      nouns.forEach(noun => {
+        let domainName = `${pronoun}${adj}${noun}.com`;
+        domainNames.push(domainName);
+      });
+    });
+  });
+
+  let domainList = document.getElementById('domainList');
+  domainList.innerHTML = '';
+  domainNames.forEach(domain => {
+    let listItem = document.createElement('li');
+    listItem.textContent = domain;
+    domainList.appendChild(listItem);
+  });
+}
+
+document.getElementById('generateButton').addEventListener('click', generateDomainNames);
+
+export { generateDomainNames }; // Export the function to be used as a module
